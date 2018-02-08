55fd72dbea612687a81d050849e417b7b4ff99bd
iliyazUI <iliyazUI@github.com>
2018-02-08 15:39:09 +0530

    updating server

--- server.js
+++ server.js
@@ -4,8 +4,8 @@ const path = require('path');
 
 app.use(express.static(__dirname + '/dist'));
 
-app.listen(process.env.PORT || 4200,function(){
-  console.log('server is up at 4200');
+app.listen(process.env.PORT || 3000,function(){
+  console.log('server is up at 3000');
 });
 
 app.get('/*', function(req, res){
